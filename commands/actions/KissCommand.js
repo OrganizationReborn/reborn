628fb8033b841b609cbd0975c6206721201e4cb2
KanonBlanc <96389178+KanonBlanc@users.noreply.github.com>
2022-01-23 11:22:16 +0800

    modified

--- commands/actions/KissCommand.js
+++ commands/actions/KissCommand.js
@@ -8,12 +8,13 @@ module.exports = class KissCommand extends BaseCommand {
 
 async run(client, message, args) {
     //const username = message.member.user.tag;
+    const member = message.member
     const user = message.mentions.members.first();
     if(user === undefined) {
       return message.reply('Are you going to kiss an air? How creepy. `?kiss @user`')
     } else {
       const kissed = new MessageEmbed()
-      .setTitle(message.author.username + ` kissed ` + member.user)
+      .setTitle(message.author.username + ` kissed ` + member)
       .setImage('https://i.imgur.com/WVSwvm6.gif')
       .setColor('RANDOM')
       .setTimestamp()
