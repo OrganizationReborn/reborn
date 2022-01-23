3909aede2f7c30ca0f2fbf8ac3ebb34cdec28967
KanonBlanc <96389178+KanonBlanc@users.noreply.github.com>
2022-01-23 13:45:22 +0800

    modified

--- commands/actions/BullyCommand.js
+++ commands/actions/BullyCommand.js
@@ -8,25 +8,16 @@ module.exports = class BullyCommand extends BaseCommand {
 
 async run(client, message, args) {
     const user = message.mentions.users.first();
-    const bullies = require('../assets/bully.json');
-    const total = Object.keys(bullies).length
     if(user === undefined) {
       return message.reply('Are you going to bully an air? How creepy. `?bully @user`')
     } 
-      let somethingThere = message.content.split(/\s+/g).slice(1).join(" ");
-      if(!somethingThere || args.number == 'none'){
-        var random = Math.floor(Math.random() * total + 1);
-        var bully = bullies[random]
-
       const member = message.mentions.users.first().username
       const kissed = new MessageEmbed()
       .setTitle(message.author.username + ` bullied ` + member)
-      .setImage(bully.image)
-      .setFooter({text: `Bully Style No: ${random}`})
+      .setImage('https://c.tenor.com/JMRAWJgcfF0AAAAM/nagatoro-ijirinaide-nagatoro-san.git')
       .setColor('RANDOM')
       .setTimestamp()
   
       message.channel.send({embeds: [kissed]})
-    }
   }
 }
