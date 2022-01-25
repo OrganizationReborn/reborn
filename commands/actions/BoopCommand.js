f9d88e7d19672d6a0986a628bb100bcc3cc0ef71
Baby Jean Baido <babybaido18@gmail.com>
2022-01-25 13:40:15 +0800

    modified

--- commands/actions/BoopCommand.js
+++ commands/actions/BoopCommand.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const BaseCommand = require('../../utils/structures/BaseCommand');
 const { MessageEmbed, Message } = require('discord.js');
 
@@ -22,29 +21,4 @@ async run(client, message, args) {
       message.channel.send({embeds: [kissed]})
     }
   }
-=======
-const BaseCommand = require('../../utils/structures/BaseCommand');
-const { MessageEmbed, Message } = require('discord.js');
-
-module.exports = class BoopCommand extends BaseCommand {
-    constructor() {
-        super('boop', 'actions', []);
-    }
-
-async run(client, message, args) {
-    const user = message.mentions.users.first();
-    if(user === undefined) {
-      return message.reply('Are you going to boop an air? How creepy. `?boop @user`')
-    } else {
-      const member = message.mentions.users.first().username
-      const kissed = new MessageEmbed()
-      .setTitle(message.author.username + ` booped ` + member)
-      .setImage('https://c.tenor.com/4OHxyGd4qp0AAAAM/boop-nose.gif')
-      .setColor('RANDOM')
-      .setTimestamp()
-  
-      message.channel.send({embeds: [kissed]})
-    }
-  }
->>>>>>> b2a7d61e0be6a6c61b9d038cc1dbbd340a8d17ee
 }
